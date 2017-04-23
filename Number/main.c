53746cc3e287b47a31f5dfc92b21290101767b1d
Crystal <crystal@CrystaldeMacBook-Pro.local>
2017-04-23 09:38:02 +0800

    Initial Commit

---
+++ Number/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Number
+//
+//  Created by Crystal on 2017/4/23.
+//  Copyright © 2017年 Crystal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
